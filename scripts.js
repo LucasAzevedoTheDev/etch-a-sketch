dbf3f861e8efd6733f0ef629112cbd55aa2c1bb2
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-02-10 19:51:59 -0300

    Add the container div and the squares inside

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let container = document.querySelector(".container");
+let squareDiv = "";
+
+// CREATING THE SQUARES
+for(i = 0; i < 32; i++) {
+  squareDiv = document.createElement("div");
+  squareDiv.setAttribute("class", "square");
+
+  container.appendChild(squareDiv);
+}
