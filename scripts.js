3bab50030b709faebeb41f98c3053c7273f05493
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-02-13 11:39:43 -0300

    add input button

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 let container = document.querySelector(".container");
-
+let squareDiv = "";
 
 for(i = 0; i < 256; i++) {
   let squareDiv = document.createElement("div");
@@ -10,9 +10,20 @@ for(i = 0; i < 256; i++) {
   squareDiv.style.backgroundColor = "black"; 
   });
   squareDiv.addEventListener("mouseleave", () => {
-  squareDiv.style.backgroundColor = "white"; 
+  squareDiv.style.backgroundColor = "grey"; 
   });
   
   container.appendChild(squareDiv);
 }
 
+// add a button that sends a popup
+let inputButton = document.querySelector(".input");
+inputButton.onclick = () => {
+  let ouput = prompt("Please write the number of squares you'd like", "");
+  container.remove();
+  
+}
+// the popup should ask the number of squares per side
+// once entered, the existing grid should be removed
+// a new grid should be generate in the same total space
+// set a limit for the user input to max of 100 squares
