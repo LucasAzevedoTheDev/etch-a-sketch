e7fdcd41f21cee837f8d005abf4f854ee650f7ea
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-02-12 22:14:47 -0300

    Create the grid

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ let container = document.querySelector(".container");
 let squareDiv = "";
 
 // CREATING THE SQUARES
-for(i = 0; i < 32; i++) {
+for(i = 0; i < 257; i++) {
   squareDiv = document.createElement("div");
   squareDiv.setAttribute("class", "square");
 
