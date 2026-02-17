536fbbd135368303c0daf2077f51ed9e6d446320
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-02-17 15:25:43 -0300

    Fix CSS properties

--- scripts.js
+++ scripts.js
@@ -1,37 +1,7 @@
 let container = document.querySelector(".container");
-let squareDiv = "";
-
-for(i = 0; i < 256; i++) {
-  let squareDiv = document.createElement("div");
-  squareDiv.setAttribute("class", "square");
-  
-  // HOVER EFFECT
-  squareDiv.addEventListener("mouseenter", () => {
-  squareDiv.style.backgroundColor = "black"; 
-  });
-  squareDiv.addEventListener("mouseleave", () => {
-  squareDiv.style.backgroundColor = "grey"; 
-  });
-  
-  container.appendChild(squareDiv);
-}
-
 let inputButton = document.querySelector(".input");
-inputButton.onclick = () => {
-  let input = prompt("Please write the number of squares you'd like", "");
-  container.remove();
+
+function createGrid(size) {
   
-  if(input < 100) {
-    document.body.appendChild(container);
-    squareDiv = document.createElement("div");
-    squareDiv.setAttribute("class", "square");
-
-    for(i = 0; i < input; i++) {
-      container.appendChild(squareDiv);
-    }
-  }
-
-  else if(input > 100) {
-    alert("Size not supported.");
-  }
 }
+
