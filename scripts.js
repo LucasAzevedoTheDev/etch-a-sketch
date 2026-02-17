22a45f88246315ce4d1849fac8fe3f23215d0e38
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-02-17 15:33:09 -0300

    add row logic

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@ let container = document.querySelector(".container");
 let inputButton = document.querySelector(".input");
 
 function createGrid(size) {
+  container.innerHTML = "";
+
+  for (let x = 0; x < size; x++) {
+    let row = document.createElement("div");
+    row.classList.add("gridRow");
   
+
+    
 }
 
